279272ce5e4b2703ab7500ad1f976f83937de733
kimyujeong <jungyk1224@likelion.org>
2020-07-13 19:56:13 +0900

    commit

--- powstargram/settings.py
+++ powstargram/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','4mz#_d_rk@9=@fe2qxw9^a1lua-2ib=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
