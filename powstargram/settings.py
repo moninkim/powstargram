51cc798bc4855f6ae5512167d5cbd13c9eb44828
kimyujeong <jungyk1224@likelion.org>
2020-07-13 20:07:29 +0900

    commit

--- powstargram/settings.py
+++ powstargram/settings.py
@@ -80,15 +80,8 @@ WSGI_APPLICATION = 'powstargram.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'powstargram',
-        'USER': 'jung',
-        'PASSWORD': 'jung98',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
