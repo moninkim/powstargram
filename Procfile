7e0bef916eabfb037ce285e713c59c1505eea864
kimyujeong <jungyk1224@likelion.org>
2020-07-13 17:57:48 +0900

    second commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn powstargram.wsgi --log-file -
--- powstargram/settings.py
+++ powstargram/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -166,3 +167,8 @@ STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FILE_STORAGE = 'blogapp.asset_storage.MediaStorage'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
